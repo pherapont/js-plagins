f97363a9d6e8e75f396db2e99bec53e5c0d2f052
pherapont <pherapont@yandex.ru>
2020-09-14 14:18:51 +0700

    options

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-const modal = $.modal()
+const modal = $.modal({
+	title: 'Модальное окно №1',
+	content : '<p class="modal__text">Lorem ipsum dolor sit.</p><p class="modal__text">Lorem ipsum dolor sit.</p><p class="modal__text">Lorem ipsum dolor sit.</p>',
+	closable: true,
+	width : '400px'
+})
--- plagins/modal.js
+++ plagins/modal.js
@@ -1,17 +1,14 @@
-function _createModal() {
+function _createModal(options) {
 	const modal = document.createElement('div')
 	modal.classList.add('modal')
 	modal.insertAdjacentHTML('afterbegin', `
 		<div class="modal__overlay">
-			<div class="modal__window">
+			<div class="modal__window" style="width: ${options.width}">
 				<div class="modal__header">
-					<span class="modal__title">Модальное окно</span>
-					<span class="modal__close">&#10008;</span>
+					<span class="modal__title">${options.title}</span>
+					${options.closable ? '<span class="modal__close">&#10008;</span>' : ''}
 				</div>
-				<div class="modal__content">
-					<p class="modal__text">Lorem ipsum dolor sit.</p>
-					<p class="modal__text">Lorem ipsum dolor sit.</p>
-				</div>
+				<div class="modal__content">${options.content}</div>
 				<div class="modal__footer">
 					<button class="modal__btn">Ok</button>
 					<button class="modal__btn">Cancel</button>
@@ -25,13 +22,39 @@ function _createModal() {
 
 $.modal = function(options) {
 	const $modal = _createModal(options)
+	let isOpen = false
+	const ANIMATION_SPEED = 300
+	const $closeBtn = $modal.querySelector('.modal__close')
+	const $overlay = $modal.querySelector('.modal__overlay')
 	return {
 		open() {
-			$modal.classList.add('open')
+			!isOpen && $modal.classList.add('open')
+			isOpen = true
+			$closeBtn.addEventListener('click', modal.close)
+			window.addEventListener('click', function(e) {
+				if (e.target === $overlay)  {return modal.close()}
+			})
 		},
 		close() {
-			$modal.classList.remove('open')
+			if (isOpen) {
+				$modal.classList.add('close')
+				$closeBtn.removeEventListener('click', modal.close)
+				window.removeEventListener('click', function(e) {
+					if (e.target === $overlay)  {return modal.close()}
+				})
+			}
+			setTimeout(() => {
+				$modal.classList.remove('open')
+				$modal.classList.remove('close')
+				isOpen = false
+			}, ANIMATION_SPEED)
+			
 		},
-		destroy() {}
+		destroy() {
+			if ($modal) {
+				modal.close()
+				document.body.removeChild($modal)
+			}
+		}
 	}
 }
